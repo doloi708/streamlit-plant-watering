a4aa7da5e757b1f880bfb9e1110c75699c1366a7
doloi708 <loi.do@seznam.cz>
2024-08-18 12:40:14 +0200

    wip

--- test_git.py
+++ test_git.py
@@ -1,6 +1,16 @@
-import os
-os.chdir("../plant-watering-vlogs")
-os.system("git pull")
-os.system("git add .")
-os.system("git commit -m'Adding data'")
-os.system("git push")
+from git import Repo
+
+PATH_OF_GIT_REPO = r'/home/loido/git_repositories/plant-watering-vlogs/.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'Adding vlogs'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code') 
+
+git_push()
